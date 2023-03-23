806cdaee0059ef2b2e540f832a0a9a47704d454f
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-23 14:14:29 +0430

    Initialize

--- src/routes/Homepage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Home from "../components/Home";
-
-const Homepage = () => {
-    <></>
-};
-
-export default Homepage;
