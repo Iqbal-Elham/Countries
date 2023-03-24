2bb1e3784ee0af5e07752f4ce43c121df8c95f46
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-24 16:07:27 +0430

    Add style for country homepage

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getAllCountries, getRegion } from '../../redux/countriesSlice/countriesSlice';
+import './Country.scss';
 
 const Country = () => {
   const {
@@ -11,36 +12,34 @@ const Country = () => {
   );
   const dispatch = useDispatch();
 
-  // const [countryData, setCountryData] = useState([]);
-
   useEffect(() => {
     if (!countriesData.length) dispatch(getAllCountries());
-    // if (success) setCountryData(countriesData);
-    if (region) {
-      dispatch(getRegion(region));
-    }
+    if (region) dispatch(getRegion(region));
   }, [dispatch, success, region]);
 
   return (
-    <div>
+    <div className="country__list">
       {isLoading ? (
         <h1>Loading...</h1>
       ) : (
         countriesData.length > 0
         && countriesData.map((item) => (
-          <Link to={`/${item.cioc}`} key={item.name.common}>
+          <Link to={`/${item.cioc}`} key={item.name.common} className="country__container">
             <img src={item.flags.png} alt={item.flags.alt} />
-            <div>
+            <div className="country__content">
               <h3>
                 Name:
+                {' '}
                 {item.name.common}
               </h3>
               <p>
-                Population:
+                Capital:
+                {' '}
                 {item.capital}
               </p>
               <p>
                 Population:
+                {' '}
                 {item.population}
               </p>
             </div>
