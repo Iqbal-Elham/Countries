4e283ff4ce568e6fe4a97267660a167fc80c53dc
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-24 16:08:58 +0430

    Add style for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { FaHome } from 'react-icons/fa';
+import './Navbar.scss';
+import { FaHome, FaSearch } from 'react-icons/fa';
+import { FiSettings } from 'react-icons/fi';
 
 const Navbar = () => (
-  <div>
-    <Link to="/">
-      <FaHome />
-    </Link>
+  <>
+    <nav className="nav__div">
+      <Link to="/" className="nav__link">
+        <FaHome />
+      </Link>
+      <h1 className="nav__title">Countries</h1>
+      <div className="nav__icons">
+        <FaSearch />
+        <FiSettings />
+      </div>
+    </nav>
     <Outlet />
-  </div>
+  </>
 );
 
 export default Navbar;
