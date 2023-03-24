2114171094298b5880dad6a07202d8b084629d86
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-24 12:27:36 +0430

    Add all reducers and fetchings

--- src/redux/countriesSlice/countriesSlice.js
+++ src/redux/countriesSlice/countriesSlice.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const getAllCountries = createAsyncThunk('getCountries/countries', async (_, thunkAPI) => {
   try {
-    const response = await axios.get('https://restcountries.com/v3.1/all');
+    const response = await axios('https://restcountries.com/v3.1/all');
     return response.data;
   } catch (error) {
     const message = (error.response && error.response.data) || error.message;
@@ -13,7 +13,17 @@ export const getAllCountries = createAsyncThunk('getCountries/countries', async 
 
 export const getCountry = createAsyncThunk('getCountry/countries', async (code, thunkAPI) => {
   try {
-    const response = await axios.get(`https://restcountries.com/v3.1/alpha/${code}`);
+    const response = await axios(`https://restcountries.com/v3.1/alpha/${code}`);
+    return response.data;
+  } catch (error) {
+    const message = (error.response && error.response.data) || error.message;
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+export const getRegion = createAsyncThunk('getRegion/countries', async (region, thunkAPI) => {
+  try {
+    const response = await axios(`https://restcountries.com/v3.1/region/${region}`);
     return response.data;
   } catch (error) {
     const message = (error.response && error.response.data) || error.message;
@@ -25,6 +35,7 @@ const initialState = {
   isLoading: false,
   countriesData: [],
   countrySearch: [],
+  region: '',
   error: false,
   success: false,
   message: '',
@@ -33,19 +44,26 @@ const initialState = {
 export const countriesSlice = createSlice({
   name: 'countries',
   initialState,
-  reducers: {},
+  reducers: {
+    setRegion: (state, action) => ({
+      ...state,
+      region: action.payload,
+    }),
+  },
   extraReducers: (builder) => {
     builder.addCase(getAllCountries.pending, (state) => ({
       ...state,
       isLoading: true,
     }));
     builder.addCase(getAllCountries.fulfilled, (state, action) => ({
+      ...state,
       isLoading: false,
       success: true,
       countriesData: action.payload,
 
     }));
     builder.addCase(getAllCountries.rejected, (state, action) => ({
+      ...state,
       isLoading: false,
       error: true,
       message: action.payload,
@@ -56,17 +74,37 @@ export const countriesSlice = createSlice({
       isLoading: true,
     }));
     builder.addCase(getCountry.fulfilled, (state, action) => ({
+      ...state,
       isLoading: false,
       success: true,
       countrySearch: action.payload,
     }));
     builder.addCase(getCountry.rejected, (state, action) => ({
+      ...state,
       isLoading: false,
       error: true,
       message: action.payload,
       countrySearch: [],
     }));
+    builder.addCase(getRegion.pending, (state) => ({
+      ...state,
+      isLoading: true,
+    }));
+    builder.addCase(getRegion.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: false,
+      success: true,
+      countriesData: action.payload,
+    }));
+    builder.addCase(getRegion.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: true,
+      message: action.payload,
+      countriesData: [],
+    }));
   },
 });
 
 export default countriesSlice.reducer;
+export const { setRegion } = countriesSlice.actions;
