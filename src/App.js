caef7060e64137101036e05e5bafe8ed940ca81b
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-24 12:24:05 +0430

    Add routers in app file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Details from './routes/Details';
 import Homepage from './routes/Homepage';
 
