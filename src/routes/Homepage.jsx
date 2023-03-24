406e8a8ef979fea5d76f046e595babece3e69fe5
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-24 12:29:43 +0430

    Add homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+/* eslint-disable */
 import Country from '../components/Country/Country';
+import Filter from '../components/Filter/Filter';
 
 const Homepage = () => (
   <>
+    <Filter />
     <Country />
   </>
 );
